de408ec017b89f5eaf3de062b2fecdebd0d13886
“TylerLua <luangphethtyler@gmail.com>
2026-02-27 20:07:06 -0800

    Window Prototype Complete

--- main.cpp
+++ main.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+// Callback function for when the window is resized
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+	std::cout << "\033[2J\033[1;1H";
+	cout << "New width: " << width << endl << ", New height: " << height << endl;
+}
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	glfwInit();
+	// Tells me that glfw will create OpenGL context with Version 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "MyFirstWindow", NULL, NULL);
+	if (window == NULL) {
+		cout << "Failed to create GLFW window" << endl;
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// GLAD manages function pointers for OpenGL, so we want to initialize GLAD before we call any OpenGL function
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	glViewport(0, 0, 800, 600);
+
+	// Tell GLFW to call this function whenever the window is resized
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+	// Render Loop
+	while (!glfwWindowShouldClose(window)) {
+
+		// Clear the color buffer
+		glfwSwapBuffers(window);
+
+		// Check for events
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
